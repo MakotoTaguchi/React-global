1968a1f4ea55b131fe4a8a365eaeaac56820ad1b
MakotoTaguchi <83949571+MakotoTaguchi@users.noreply.github.com>
2021-12-16 20:26:50 +0900

    main

--- src/index.js
+++ src/index.js
@@ -1,11 +1,12 @@
 import ReactDOM from "react-dom";
 import React from "react";
 import { App } from "./App";
+import { AdminFlagProvider } from "./components/providers/AdminFlagProvider";
 
 const rootElement = document.getElementById("root");
 ReactDOM.render(
-  <React.StrictMode>
+  <AdminFlagProvider>
     <App />
-  </React.StrictMode>,
+  </AdminFlagProvider>,
   rootElement
 );
